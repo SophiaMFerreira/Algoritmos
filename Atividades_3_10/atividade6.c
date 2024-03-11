714f2c9d77105b4582971d34b81b0503b112e170
SophiaMFerreira <151583346+SophiaMFerreira@users.noreply.github.com>
2024-03-11 11:29:07 -0300

    Update atividade6.c
    camelCase

--- Atividades_3_10/atividade6.c
+++ Atividades_3_10/atividade6.c
@@ -1,4 +1,4 @@
-/**6) Determinar a média ponderada das três notas de um aluno. Considerar que os pesos das notas são 2, 3 e 5, respectivamente**/
+/**6) Determinar a mÃ©dia ponderada das trÃªs notas de um aluno. Considerar que os pesos das notas sÃ£o 2, 3 e 5, respectivamente**/
 
 #include <stdio.h>
 #include <locale.h>
@@ -6,19 +6,19 @@
 main (){
     setlocale(LC_ALL, "Portuguese");
 
-    float nota_1, nota_2, nota_3, soma, media_p;
-    int peso_2 = 2;
-    int peso_3 = 3;
-    int peso_5 = 5;
+    float nota1, nota2, nota3, soma, mediaP;
+    int peso2 = 2;
+    int peso3 = 3;
+    int peso5 = 5;
 
-    printf("Informe a 1ª nota: \n");
-    scanf("%f", &nota_1);
-    printf("Informe a 2ª nota: \n");
-    scanf("%f", &nota_2);
-    printf("Informe a 3ª nota: \n");
-    scanf("%f", &nota_3);
-    soma = (nota_1 * peso_2) + (nota_2 * peso_3) + (nota_3* peso_5);
-    media_p = soma/ 3;
+    printf("Informe a 1Âª nota: \n");
+    scanf("%f", &nota1);
+    printf("Informe a 2Âª nota: \n");
+    scanf("%f", &nota2);
+    printf("Informe a 3Âª nota: \n");
+    scanf("%f", &nota3);
+    soma = (nota1 * peso2) + (nota2 * peso3) + (nota3* peso5);
+    mediaP = soma/ 3;
 
-    printf("A média ponderada das notas: %.2f, %.2f e %.2f é igual a %.2f", nota_1, nota_2, nota_3, media_p);
+    printf("A mÃ©dia ponderada das notas: %.2f, %.2f e %.2f Ã© igual a %.2f", nota1, nota2, nota3, mediaP);
 }
